785f28f80663b74648aeeea4bf460b661d09ec6f
wc_earlymor <1162907534@qq.com>
2023-04-23 22:33:51 +0800

    thread

---
+++ pthread.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+void* tfn(void*arg){
+    printf("thread: pid=%d tid=%lu\n",getpid(),pthread_self());//thread: pid=12912 tid=140685636388544
+    return NULL;
+}
+int main(){
+    pthread_t tid;
+    printf("pid: %d tid:%lu\n",getpid(),pthread_self());//pid: 12912 tid:140685638399616
+    int ret=pthread_create(&tid,NULL,tfn,NULL);
+    if(ret!=0){
+        perror("pthread_create:");
+    }
+    sleep(1);
+    return 0;
+}
