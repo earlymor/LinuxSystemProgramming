42fb12e654eeb98fedfdb64af07e614362176286
wc_earlymor <1162907534@qq.com>
2023-04-24 21:36:27 +0800

    loop

---
+++ loop_create.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+void *tfn(void *arg) {
+  printf("%dth thread pid: %d tid: %lu\n", (int)arg + 1, getpid(),
+         pthread_self());
+}
+int main() {
+  pthread_t tid;
+  int ret;
+  int i;
+  for (i = 0; i < 5; i++) {
+    ret = pthread_create(&tid, NULL, tfn, (void *)i);//发生用户态与内核态转变
+    if (ret != 0) {
+      perror("pthread_create");
+      exit(1);
+    }
+  }
+  sleep(i);
+  printf("main pid:%d tid:%lu\n", getpid(), pthread_self());
+  return 0;
+}
--- pthread.c
+++ pthread.c
@@ -8,10 +8,10 @@ void* tfn(void*arg){
 int main(){
     pthread_t tid;
     printf("pid: %d tid:%lu\n",getpid(),pthread_self());//pid: 12912 tid:140685638399616
-    int ret=pthread_create(&tid,NULL,tfn,NULL);
+    int ret=pthread_create(&tid,NULL,tfn,NULL);//成功返回0,失败返回errno
     if(ret!=0){
         perror("pthread_create:");
     }
-    sleep(1);
+    sleep(1);//return 后销毁共用的内存
     return 0;
 }
